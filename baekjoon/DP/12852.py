b660b3c62341c6e964ceedd2526530bb760fb414
psjj960507@gmail.com <psjj960507@gmail.com>
2024-02-02 15:28:53 +0900

    02/02

---
+++ baekjoon/DP/12852.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+path = [0] * (N + 1)
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    path[i] = i - 1
+
+    if i % 2 == 0 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+        path[i] = i // 2
+    
+    if i % 3 == 0 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+        path[i] = i // 3
+
+
+print(dp[N])
+answer = []
+while N > 0:
+    answer.append(N)
+    N = path[N]
+print(' '.join(map(str, answer)))
