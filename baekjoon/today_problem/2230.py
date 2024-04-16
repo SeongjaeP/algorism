26a49d25e457899490f03858291be758723c1ba6
SeongJaePark <psjj960507@gmail.com>
2024-04-16 19:59:17 +0900

    투 포인터

---
+++ baekjoon/today_problem/2230.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+num_list = list(int(input()) for _ in range(n))
+
+
+#result = []
+num_list.sort()
+result = float('inf')
+
+left = 0
+right = 0
+
+while right < n:
+    if num_list[right] - num_list[left] >= m:
+        result = min(result, num_list[right] - num_list[left])
+        left += 1
+    else:
+        right += 1
+
+print(result)
+            
+
+
+
+
+            # 1 3 5 
+            # 3 5
